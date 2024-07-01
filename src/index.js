ff931f5ec59b52a5917f6644ae0db8bf173bc33c
ZiaG22 <ziaove22@gmail.com>
2024-06-30 20:46:54 -0500

    add js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 
+let date = new Date();
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+let timeElement = document.querySelector("#time");
+timeElement.innerHTML = formatDate(date);
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
